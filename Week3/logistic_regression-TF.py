6ea2e3d75b51e25001908f6fb5a4e76de7f7e4ae
ngctnnnn <ngctnnnn@gmail.com>
2021-09-30 15:23:42 +0700

    fix logistic

--- Week3/logistic_regression-TF.py
+++ Week3/logistic_regression-TF.py
@@ -1,22 +1,19 @@
 import numpy as np
 import pandas as pd
 import tensorflow as tf
-from tensorflow.keras import layers
 import matplotlib.pyplot as plt
 from tensorflow import keras
-from sklearn.utils import shuffle
+from tensorflow.keras import layers
 
-S1 = np.array([8, 0])
-S2 = np.array([2, 5])
+S1= np.array([8, 0])
+S2= np.array([2, 5])
 
 n_sample = 10
 red_points = S1 + np.random.normal(0, 1.5, size=(n_sample, 2))
 blue_points = S2 + np.random.normal(0, 1.5, size=(n_sample, 2))
-
-@tf.function
-def sigmoid(Z):
-    return 1/(1 + tf.math.exp(-Z))
                                                                                  
+eps = 0.01
+
 plt.plot(red_points[:, 0], red_points[:, 1], 'ro')
 plt.plot(blue_points[:, 0], blue_points[:, 1], 'bo')
 
@@ -26,35 +23,34 @@ ones = np.ones(n_sample)
 zeros = np.zeros(n_sample)
 
 y_data = np.concatenate([ones, zeros])
-y_data = shuffle(y_data)
+
 
 def get_model():
     inputs = keras.Input(shape=(2, ))
     outputs = layers.Dense(1, activation='sigmoid')(inputs)
     return keras.Model(inputs=inputs, outputs=outputs)
 
-opt = tf.optimizers.SGD(0.1)
+optimizer = tf.optimizers.SGD(0.1)
 model = get_model()
-eps = 1e-3
 
 while True:
     with tf.GradientTape() as tape:
-        X_data = tf.cast(X_data, tf.float32)
-        Y_data = tf.cast(y_data.reshape(-1,1), tf.float32)
+        X_data_ = tf.cast(X_data, tf.float32)
+        y_data_ = tf.cast(y_data.reshape(-1,1), tf.float32)
 
-        prediction = model(X_data)
-        
-        '''Loss function'''
-        loss = keras.losses.binary_crossentropy(Y_data, prediction)
+        pred = model(X_data_)
+        loss = keras.losses.binary_crossentropy(y_data_, pred)
         grads = tape.gradient(loss, model.trainable_variables)
-         
-    opt.apply_gradients(zip(grads, model.trainable_variables))
-    y = model.trainable_variables[0][0][0].numpy() * X_data + model.trainable_variables[0][1][0]
+ 
+    optimizer.apply_gradients(zip(grads, model.trainable_variables))
+
+    x_vis = X_data
+    y_vis = model.trainable_variables[0][0].numpy() * x_vis + model.trainable_variables[0][1]
     
-    plt.plot(X_data, y)
-    plt.pause(0.05)
+    plt.plot(x_vis, y_vis)
+    plt.pause(0.01)
     
-    if abs(grads[0][0][0].numpy()) < eps and abs(grads[0][1][0].numpy()) < eps:
+    if abs(grads[0][0].numpy()) < eps and abs(grads[0][1].numpy()) < eps:
         break
 
 print('Theta toi tu: ', model.trainable_variables)
